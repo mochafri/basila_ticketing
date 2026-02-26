92d79724219f89b11af56d844c7f25df11c3f4a8
mochafri <afrizalmuhammad656@gmail.com>
2026-02-26 17:13:43 +0700

    Responsive navbar done

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -1,33 +1,37 @@
+<nav class="navbar navbar-expand-lg navbar-header border-bottom px-3">
 
+    <div class="container-fluid">
 
-    <div class="navbar-header border-bottom px-3 w-100 d-flex justify-content-between align-items-center">
-
-        <!-- LEFT -->
-        <div class="d-flex align-items-center gap-3">
-
-            <button type="button" class="sidebar-toggle btn btn-sm btn-outline-light">
-                ☰
-            </button>
-
-            <input type="text" class="form-control form-control-sm" placeholder="Search" style="width:200px;">
+        <!-- SEARCH (Kiri) -->
+        <div class="d-flex align-items-center">
+            <input type="text" class="form-control form-control-sm d-none d-lg-block" placeholder="Search"
+                style="width:200px;">
         </div>
 
-        <!-- RIGHT -->
-        <div class="d-flex align-items-center gap-3">
+        <!-- RIGHT SIDE -->
+        <div class="d-flex align-items-center gap-3 ms-auto">
 
-            <button class="btn btn-sm text-light">
-                <iconify-icon icon="si:sun-duotone" width="20" height="20"></iconify-icon>
+            <!-- Dark mode -->
+            <button class="btn btn-sm text-light d-none d-md-block">
+                <iconify-icon icon="si:sun-duotone" width="20"></iconify-icon>
             </button>
 
-            <button class="btn btn-sm text-light">
-                <iconify-icon icon="material-symbols-light:language" width="20" height="20"></iconify-icon>
+            <!-- Language -->
+            <button class="btn btn-sm text-light d-none d-md-block">
+                <iconify-icon icon="material-symbols-light:language" width="20"></iconify-icon>
             </button>
 
-            <img src="<?= base_url('assets/images/user.png') ?>" width="35" height="35" class="rounded-circle"
-                style="object-fit:cover;">
+            <!-- User -->
+            <img src="<?= base_url('assets/images/user.png') ?>" width="35" height="35"
+                class="rounded-circle d-none d-md-block" id="logo" style="object-fit:cover;">
+
+            <!-- Sidebar Toggle (PALING KANAN) -->
+            <button type="button" class="sidebar-toggle btn btn-sm btn-outline-light d-lg-none">
+                ☰
+            </button>
 
         </div>
 
     </div>
 
-<script src="<?= base_url('assets/js/sidebar.js') ?>"></script>
+</nav>
