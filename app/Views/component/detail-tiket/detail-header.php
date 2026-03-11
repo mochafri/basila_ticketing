1e2d11ad1f9808dc1126390cf8bb420d3f64f723
mochafri <afrizalmuhammad656@gmail.com>
2026-03-11 12:28:13 +0700

    detail-tiket : done sementara

---
+++ app/Views/component/detail-tiket/detail-header.php
@@ -0,0 +1,28 @@
+<div class="card mb-4 border-0">
+    <div class="card-header p-5 pb-3 bg-transparent">
+        <div class="card-title">
+            <h2 class="text-uppercase fw-bold" style="letter-spacing: -1px;">Pengajuan registrasi mk terlambat</h2>
+            <div class="d-flex gap-4 align-items-center mt-3">
+                <div class="d-flex align-items-center gap-2">
+                    <iconify-icon icon="basil:stack-outline" class="text-danger"></iconify-icon>
+                    <span class="text-uppercase fw-medium custom-text">registasi / registrasi
+                        mata kuliah</span>
+                </div>
+                <div class="d-flex align-items-center gap-2">
+                    <iconify-icon icon="bi:person" class="text-danger"></iconify-icon>
+                    <span class="text-uppercase fw-medium custom-text" >oleh : staff
+                        user</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card-body p-5 d-flex flex-column gap-4 task-desc">
+        <p class="fs-5 fw-medium">Mahasiswa angkatan 2021 ingin melakukan registrasi susulan untuk semester
+            ganjil dengan alasan tertentu.</p>
+        <span class="text-uppercase custom-text" >dokumen pendukung</span>
+        <div class="btn btn-light col-3 d-flex align-items-center gap-2 justify-content-center">
+            <iconify-icon icon="fluent:document-20-regular"></iconify-icon>
+            <span class="custom-small-font">KTM_MHS.PDF</span>
+        </div>
+    </div>
+</div>
