eccd82c46071597e70d7ccd46fbbe050f6387ea9
ilhom <rilhamgojali123@gmail.com>
2026-04-10 08:54:09 +0700

    membuat mapping kaur dan staff nya

---
+++ app/Database/Migrations/2026-04-07-155741_MappingKabag.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class MappingKabag extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'unsigned' => true,
+                'auto_increment' => true
+            ],
+            'nip_kaur' => [
+                'type'=> 'VARCHAR',
+                'constraint' => '255'
+            ],
+            'nama_kaur' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255'
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('kaurs');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('kaurs');
+    }
+}
---
+++ app/Services/KaurStaffService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services;
+
+use \App\Models\Kaur;
+use \App\Models\Staff;
+
+class KaurStaffService
+{
+    protected $kaur;
+    protected $staff;
+
+    public function __construct()
+    {
+        $this->kaur = new Kaur();
+        $this->staff = new Staff();
+    }
+
+    public function getKaur()
+    {
+        $getKaur = $this->kaur->findAll();
+        return $getKaur;
+    }
+
+    public function getStaff($nip)
+    {
+        $getId = $this->kaur->where('nip_kaur', $nip)->first();
+
+        if (!isset($getId)) {
+            return null;
+        }
+
+        $getStaff = $this->staff->where('fk_kaur', $getId['id'])->findAll();
+
+        if (!isset($getStaff)) {
+            return null;
+        }
+
+        return $getStaff;
+    }
+}
