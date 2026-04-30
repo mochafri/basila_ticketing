f1ee92177781eace3752a0f32b839686e4e87ee0
ilhom <rilhamgojali123@gmail.com>
2026-04-30 22:04:53 +0700

    Feat:Menghilangkan text durasi pengerjaan

--- app/Views/component/detail-tiket/ticket_created.php
+++ app/Views/component/detail-tiket/ticket_created.php
@@ -8,11 +8,7 @@
         <div class="d-flex flex-wrap gap-2 mt-1">
             <span class="custom-text text-muted small d-flex align-items-center gap-1">
                 <iconify-icon icon="ph:check-circle-bold" class="text-success"></iconify-icon>
-                Selesai: <?= format_datetime_indo($detail['completed_at'] ?? $detail['created_at']) ?>
-            </span>
-            <span class="custom-text text-muted small d-flex align-items-center gap-1" title="Durasi dihitung dari waktu mulai hingga selesai" data-bs-toggle="tooltip">
-                <iconify-icon icon="ph:timer-bold" class="text-primary"></iconify-icon>
-                Durasi: <?= format_duration($detail['created_at'], $detail['completed_at']) ?>
+                Diajukan: <?= format_datetime_indo($detail['created_at']) ?>
             </span>
         </div>
     </div>
